63ec236b4bee015d215bd33952d22bcfcec3a4bb
Wiebe <wiebe@kloosterman.eu>
2024-09-08 15:08:04 -0400

    refactor: update update process

--- Lib/base.py
+++ Lib/base.py
@@ -5,3 +5,16 @@ class Base():
     @staticmethod
     def snakeCaseToPascalCase(test_str):
         return string.capwords(test_str.replace("_", " ")).replace(" ", "")
+
+    
+    # removes suffix from the end of string s
+    @staticmethod
+    def rchop(s, suffix):
+        if suffix and s.endswith(suffix):
+            return s[:-len(suffix)]
+        return s
+    
+    # removes prefix from the start of string str
+    @staticmethod
+    def lchop(str, prefix):
+        return str.lstrip(prefix)
