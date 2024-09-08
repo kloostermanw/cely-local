2aac4d47be9b573a31fa635009734e0f75d71cf2
Wiebe <wiebe@kloosterman.eu>
2024-09-08 13:48:56 -0400

    refactor: update .gitrelease

--- Commands/update_check.py
+++ Commands/update_check.py
@@ -1,10 +1,10 @@
-from Commands.command import Command
+from Commands.update import Update
 
 # pip install gitpython
 import git
 import os
 
-class UpdateCheck(Command):
+class UpdateCheck(Update):
 
     def configure(self):
         self.name = "update:check";
@@ -25,38 +25,9 @@ class UpdateCheck(Command):
         # git rev-list ${local}..${upstream} --count
         count = repo.git.rev_list('develop..origin/develop', '--count')
 
-
         tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
+        tagList=repo.git.ls_remote("--tags", "orgin")
         print(tags[-1])
-
-
+        print(tagList[-1])
 
         return int(count) > 0
-
-    def getRepo(self):
-        blnVerbose = False
-        sourceBranch = 'develop'
-
-        if (os.path.exists(".git") == False):
-            print(".git not found.")
-            exit(0)
-
-        repo = git.Repo(search_parent_directories=True)
-        branch = repo.active_branch
-
-        if branch.name == sourceBranch:
-            if blnVerbose:
-                print(branch.name)
-        else:
-            print('no ' + sourceBranch + ' branch found' +  ' but ' + branch.name)
-            exit(0)
-
-        if repo.untracked_files:
-            print(sourceBranch + ' branch has untracked files')
-            exit(0)
-
-        # if repo.is_dirty():
-        #     print(sourceBranch + ' branch has uncommitted files')
-        #     exit(0)
-        
-        return repo
