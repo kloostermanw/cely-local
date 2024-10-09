a22ed02b1d1e33f5f0846f9c0ab34115f829fcbd
Wiebe <wiebe@celerypayroll.com>
2024-10-09 08:05:22 -0400

    refactor: added health

--- Commands/command.py
+++ Commands/command.py
@@ -1,3 +1,6 @@
+import configparser
+import os.path
+
 class Command:
     def __init__(self):
         self.configure()
@@ -5,6 +8,7 @@ class Command:
     def configure(self):
         self.name = "";
         self.description = "";
+        self.config = "";
 
     def getCommand(self):
         return self.name
@@ -17,3 +21,17 @@ class Command:
             "command": self.name,
             "description": self.description
         }
+    
+    def getConfig(self):
+        config = configparser.ConfigParser();
+        config.sections();
+        file1 = 'config/' + self.config + '/conf.default';
+        file2 = 'config/' + self.config + '/conf';
+
+        if (os.path.isfile(file2)):
+            config.read([file1, file2])
+            print('exist')
+        else:
+            config.read(file1);
+            print('not exist')
+        return config;
--- Commands/health.py
+++ Commands/health.py
@@ -1,10 +1,36 @@
 from Commands.command import Command
+import configparser
+import git
 
 class Health(Command):
 
     def configure(self):
         self.name = "local:health";
         self.description = "check the health of the current celery environment.";
+        self.config = "health";
 
     def handle(self, args):
-        print('main')
+        config = self.getConfig();
+        default = config['DEFAULT'];
+
+        list = default.get('list').split(',');
+        
+        for r in list:
+            dir = default.get(r);
+            self.gitCheck(r, dir);
+
+    def gitCheck(self, r, dir):
+        repo = git.Repo(dir, search_parent_directories=True)
+        branch = repo.active_branch
+
+        # Fetch all remotes
+        for remote in repo.remotes:
+            remote.fetch()
+
+        local='develop'
+        upstream='origin/develop'
+
+        ahead = repo.git.rev_list(upstream + '..' + local, count=True)
+        behind = repo.git.rev_list(local + '..' + upstream, count=True)
+
+        print("%3s %3s  %5s" % (ahead, behind, dir));
